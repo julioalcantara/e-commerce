51f99b9f513f04f88bda776e657497d38c18ff62
julioalcantara <alcantara.juliosousa@gmail.com>
2020-06-09 18:04:43 +0100

    boken the homepage into smaller components, finished styling our menu-item component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './homepage.component';
-import HomePange from './homepage.component';
+import HomePange from './pages/homepage/homepage.component';
 
 function App() {
   return (
