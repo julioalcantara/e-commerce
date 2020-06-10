a37a8329e45771d70c3d309575ee1a89232a4dbe
julioalcantara <alcantara.juliosousa@gmail.com>
2020-06-10 11:01:52 +0100

    adding header and links to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import './App.css';
 
 import HomePange from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.components';
+import Header from './componets/header/header.component';
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact={true} path='/' component={HomePange} />
         <Route exact={true} path='/shop' component={ShopPage} />
