38e5b845ea5728ef265889aaa933855202c12471
julioalcantara <alcantara.juliosousa@gmail.com>
2020-06-12 20:44:38 +0100

    Finishing sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -31,8 +31,6 @@ class App extends Component {
               id: snapshot.id,
               ...snapshot.data()
             }
-          }, () => {
-            console.log(this.state);
           }) 
         });
       } else {
