9cc57d6182216cf278b3dc7670abe00a85e3ab88
julioalcantara <alcantara.juliosousa@gmail.com>
2020-06-17 20:28:46 +0100

    added checkout item component and updated cart to use selectors

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
+import checkoutItem from '../../componets/checkout-item/checkout-item.component';
+
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
 import './checkout.styles.scss';
+import CheckoutItem from '../../componets/checkout-item/checkout-item.component';
 
 const CheckoutPage = ({cartItems, total}) => (
     <div className='checkout-page'>
@@ -26,10 +29,10 @@ const CheckoutPage = ({cartItems, total}) => (
             </div>
         </div>
         {
-            cartItems.map(cartItem =>
-                cartItem.name    
-            )
-        }
+            cartItems.map(cartItem => (
+                <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+            ))}
+
         <div className='total'>
             <span>TOTAL: €{total}</span>
         </div>
