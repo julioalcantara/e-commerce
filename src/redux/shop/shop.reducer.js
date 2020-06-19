27e6348798a9aeee16907ece9e0ff4bace970342
julioalcantara <alcantara.juliosousa@gmail.com>
2020-06-19 20:45:33 +0100

    Brought shop data to the app, added shop data to redux

--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,11 +1,18 @@
 import SHOP_DATA from './shop.data';
 
+import ShopActionsType from './shop.types';
+
 const INITIAL_STATE = {
   collections: SHOP_DATA
 };
 
 const shopReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case ShopActionsType.UPDATE_COLLECTION:
+      return {
+        ...state,
+        collection: action.payload
+      }
     default:
       return state;
   }
---
+++ src/redux/shop/shop.types.js
@@ -0,0 +1,5 @@
+const ShopActionsType = {
+    UPDATE_COLLECTION: 'UPDATE_COLLECTIONS'
+}
+
+export default ShopActionsType;
