388c9331a66a170e5fa6d332b003086cac9e6144
julioalcantara <alcantara.juliosousa@gmail.com>
2020-11-09 17:23:51 +0000

    fixing bug during PWA

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(enforce.HTTPS({ trustProtoHeader: true}));
   app.use(express.static(path.join(__dirname, 'client/build')));
 
+  app.get('/service-worker.js', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+  });
+
   app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
@@ -33,7 +37,7 @@ app.listen(port, error => {
 });
 
 app.get('/service-worker.js', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client/build', 'service-worker.js'));
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
 });
 
 app.post('/payment', (req, res) => {
